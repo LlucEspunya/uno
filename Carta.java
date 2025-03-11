634d467ba10acb3ba6e92a60893c45bf0ddb1fd6
LlucEspunya <lespuna562@boscdelacoma.cat>
2025-03-11 12:32:16 -0700

    Creació classe Carta

--- Carta.java
+++ Carta.java
@@ -1,4 +1,21 @@
 package UNO;
 
 public class Carta {
+    public enum Color{
+        vermell,
+        groc,
+        verd,
+        blau
+    }
+
+    private int numeroCarta;
+    private Color colorCarta;
+
+    public Color getColor(){
+        return colorCarta;
+    }
+
+    public int getNumero(){
+        return numeroCarta;
+    }
 }
